30a53a3018e7593846096f2acb9c5f78b4701ffd
Johnakins <johnolawole1@gmail.com>
2023-12-14 03:22:10 +0100

    get all timeline posts of user and friends

--- routes/post.js
+++ routes/post.js
@@ -1,4 +1,5 @@
-const Post = require("../models/Post")
+const Post = require("../models/Post") 
+const User = require("../models/User") 
 const router = require("express").Router()
 
 //create a post
@@ -71,8 +72,17 @@ router.get('/:id', async (req,res)=>{
 })
 
 //get timeline post
-router.get('/timeline', async (req,res)=>{
-
+router.get('/timeline/all', async (req,res)=>{
+    try {
+        const currentUser = await User.findById(req.body.userId)
+        const userPosts = await Post.find({userId: currentUser._id})
+        const friendPosts = await Promise.all(currentUser.followings.map((friendId)=>{
+            return Post.find({userId: friendId})
+        }))
+        res.status(200).json(userPosts.concat(...friendPosts))
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
 module.exports = router
